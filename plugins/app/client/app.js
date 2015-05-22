ffde0b06d527d5a6e745236bd637a8dcfd29b7b8
Fernando Serrano Carpena <ferserc1@gmail.com>
2015-05-22 11:41:35 +0200

    Multilanguage support added

--- plugins/app/client/app.js
+++ plugins/app/client/app.js
@@ -2,16 +2,28 @@
 (function() {
 	var app = angular.module('paellaserver', [
 		"ngRoute",
+		'pascalprecht.translate',
 		"catalogModule",
 		"loginModule"
 	]);
 
-	app.config(["$routeProvider",
-		function($routeProvider) {
+	app.config(["$routeProvider","$translateProvider",
+		function($routeProvider,$translateProvider) {
 			$routeProvider
 				.when('/', {
 					redirectTo:'/catalog'
 				});
+
+			function loadDictionary(localization) {
+				$.ajax('app/i18n/' + localization + '.json')
+					.success(function(data) {
+						$translateProvider.translations(localization,data);
+					});
+			}
+
+			loadDictionary('es');
+			loadDictionary('en');
+			$translateProvider.preferredLanguage('es');
 		}]);
 
 })();
