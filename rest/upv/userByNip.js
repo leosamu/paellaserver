7190d63b771cd48139f95d3567b30ad5986783ba
Fernando Serrano Carpena <ferserc1@gmail.com>
2015-10-23 12:05:30 +0200

    New role added to upv requests

--- rest/upv/userByNip.js
+++ rest/upv/userByNip.js
@@ -7,7 +7,7 @@ var UserController = require(__dirname + '/../../controllers/user');
 exports.routes = {
 	search: { param:'search', get:[
 		AuthController.EnsureAuthenticatedOrDigest,
-		AuthController.CheckRole(['ADMIN',"YOUTUBE"]),
+		AuthController.CheckRole(['ADMIN',"YOUTUBE",'RIUNET']),
 		UserController.Search('auth.UPV.nip','number'),
 		CommonController.JsonResponse
 	]}
