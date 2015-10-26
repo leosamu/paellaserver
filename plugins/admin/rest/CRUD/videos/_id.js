2d468e0faf3c4653cef875d6e0be9600ee25b70f
Miguel Escriva <miesgre@gmail.com>
2015-10-26 11:04:51 +0100

    videos and channels patch method implemented

--- plugins/admin/rest/CRUD/videos/_id.js
+++ plugins/admin/rest/CRUD/videos/_id.js
@@ -38,7 +38,15 @@ exports.routes = {
 		patch: [
 			AuthController.CheckRole(['ADMIN']),
 			function(req,res) {			
-				res.sendStatus(500);
+				Video.findByIdAndUpdate({"_id": req.params.id }, req.body, function(err, item) {
+					if(err) { return res.sendStatus(500); }
+					if (item) {
+						res.status(204).send(item);
+					}
+					else {
+						res.sendStatus(500);
+					}
+				});
 			}
 		]
 	}
