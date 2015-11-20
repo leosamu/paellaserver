e9522660c50843cf8cec833e5579856a3a9d0523
Miguel Escriva <miesgre@gmail.com>
2015-11-20 11:18:41 +0100

    paella fix bug

--- client/player/javascript/paella_player.js
+++ client/player/javascript/paella_player.js
@@ -6882,6 +6882,11 @@ Class ("paella.editor.Timeline", {
 		this.loadPlugins();
 
 		this.setupCursors();
+		
+		var self = this;
+		paella.events.bind(paella.events.documentChanged,function(event,params) {
+			self.rebuildAllTracks();
+		});		
 	},
 
 	setupCursors:function() {
@@ -6960,6 +6965,12 @@ Class ("paella.editor.Timeline", {
 		this.selectTrackList(this.currentTrackList);
 	},
 
+	rebuildAllTracks: function() {
+		this.trackItemList.forEach(function(track) {
+			track.rebuild()
+		});		
+	},
+	
 	rebuildTrack:function(pluginName) {
 		var plugins = paella.editor.pluginManager.trackPlugins;
 		for (var i=0; i<this.trackItemList.length; ++i) {
