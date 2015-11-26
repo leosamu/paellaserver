67d50b4d7c004c739244afd030f42b6ecec58f7b
Miguel Escriva <miesgre@gmail.com>
2015-11-26 10:52:03 +0100

    creating a new channel action

--- plugins/admin-plugin-videos/client/channel-actions.js
+++ plugins/admin-plugin-videos/client/channel-actions.js
@@ -2,40 +2,53 @@
 	var app = angular.module('adminPluginVideos');
 	
 	
-	app.run(['Actions', '$q', '$modal', function(Actions, $q, $modal) {
+	app.run(['Actions', '$q', 'ChannelCRUD', '$modal', function(Actions, $q, ChannelCRUD, $modal) {
 
 		Actions.registerAction(
 			{
-				label: "test",
+				label: "Get link to videos",
 				context: "channel",
 				beforeRun: function(items) {
 					var deferred = $q.defer();
-										
-					var modalInstance = $modal.open({
-						template: "<h1>Hola</h1>"
+					
+					var promises = [];
+					items.forEach(function(ch){
+						var p = ChannelCRUD.get({id: ch._id}).$promise;
+						promises.push(p);
+					});
+					
+					$q.all(promises)
+					.then(function(resolve){
+						var videos = [];
+						var html = "<h1>Listado de videos</h1><ul>";
+						resolve.forEach(function(ch){						
+							if (ch.videos) {
+								ch.videos.forEach(function(v){
+									html = html + "<li>https://media.upv.es/player/?id=" + v._id + '</li>';
+									console.log(v._id);								
+								});
+							}
+						});
+						
+						html = html + '</ul>';
+						var modalInstance = $modal.open({
+							template: html
+						});						
 					});
 										
-					deferred.resolve({});
 					
-					return modalInstance.result;					
-					//return deferred.promise;					
+					//deferred.resolve({});
+					
+//					return modalInstance.result;					
+					return deferred.promise;					
 				},
 				
 				runAction: function(item, params) {
-					console.log(params);
-					var ms = Math.floor((Math.random() * 10000) + 1);
-				
 					var deferred = $q.defer();
-					setTimeout(function() {
-						console.log("TODO: test " + item._id);
-						deferred.resolve("");
-					}, ms);
-					
+					deferred.resolve();
 					return deferred.promise;
 				}
-				
 			}
 		);
-				
 	}]);	
 })();
