d8a726077c1fe5c2c5e154bc1442103073e209c8
Miguel Escriva <miesgre@gmail.com>
2015-12-03 12:50:51 +0100

    action: link to videos

---
+++ plugins/admin-plugin-videos/client/video-action-link-to-videos.js
@@ -0,0 +1,51 @@
+(function() {
+	var app = angular.module('adminPluginVideos');
+	
+	
+	app.run(['Actions', '$q', '$modal', function(Actions, $q, $modal) {
+
+		Actions.registerAction(
+			{
+				label: "Get link to videos",
+				context: "video",
+				beforeRun: function(items) {
+					var deferred = $q.defer();
+					
+					var videos = [];
+					items.forEach(function(v) {
+						videos.push(v._id);
+					});
+					
+					var modalInstance = $modal.open({
+						templateUrl:'admin-plugin-videos/views/modal/video-links.html',
+						controller:['$scope', '$modalInstance', 'videos', function($scope, $modalInstance, videos){
+							$scope.videos = videos;
+
+							$scope.accept = function () {
+								$modalInstance.close();
+							};								
+						}],
+						resolve:{
+							videos: function() {
+								return videos;
+							}
+						},
+						backdrop: true
+					});
+		
+					modalInstance.result.finally(function(result) {
+						deferred.reject();
+					});
+													
+					return deferred.promise;					
+				},
+				
+				runAction: function(item, params) {
+					var deferred = $q.defer();
+					deferred.resolve();
+					return deferred.promise;
+				}
+			}
+		);
+	}]);	
+})();
