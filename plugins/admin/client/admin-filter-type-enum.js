3f48bb3478b7deb8097b1a9cc1d1974436fd5ea6
Miguel Escriva <miesgre@gmail.com>
2016-02-08 12:06:37 +0100

    Change catalog filter

--- plugins/admin/client/admin-filter-type-enum.js
+++ plugins/admin/client/admin-filter-type-enum.js
@@ -11,6 +11,15 @@
 			templateUrl: 'admin/views/directives/admin-filter-type-enum.html',
 			controller: ["$scope", function($scope) {
 			
+				if (typeof $scope.filter.values === "function") {
+					$scope.filter.values().then(function(v){
+						$scope.values = v;
+					})
+				}
+				else if ($scope.filter.values instanceof Array) {
+					$scope.values = $scope.filter.values;
+				}
+			
 				$scope.addEnumFilter = function() {
 					$scope.addFilter({
 						params: $scope.filterParam
