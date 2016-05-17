5ed86d64ab04eb116af98b13cd076cb3e1c2d547
Miguel Escriva <miesgre@gmail.com>
2016-05-17 13:10:09 +0200

    secondsToHuman filter added

--- plugins/app/client/filter-bytes.js
+++ plugins/app/client/filter-bytes.js
@@ -29,4 +29,33 @@
 		};
 	});	
 	
+	
+	app.filter('secondsToHuman', function() {
+	
+		var secondsTohhmmss = function(totalSeconds) {
+			var hours   = Math.floor(totalSeconds / 3600);
+			var minutes = Math.floor((totalSeconds - (hours * 3600)) / 60);
+			var seconds = totalSeconds - (hours * 3600) - (minutes * 60);
+			
+			// round seconds
+			seconds = Math.round(seconds * 100) / 100
+			
+			var result = (hours < 10 ? "0" + hours : hours);
+			  result += ":" + (minutes < 10 ? "0" + minutes : minutes);
+			  result += ":" + (seconds  < 10 ? "0" + seconds : seconds);
+			return result;
+		}	
+	
+	
+		return function(seconds) {
+			if (!seconds) {
+				return "-"
+			}
+			
+			return secondsTohhmmss(seconds);
+		};
+	});	
+	
+	
+	
 })();
