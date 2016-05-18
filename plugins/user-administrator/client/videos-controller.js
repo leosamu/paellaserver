caa390f19c4c2c97eaf0d7b0b60cb47e5b742026
Miguel Escriva <miesgre@gmail.com>
2016-05-18 12:49:16 +0200

    user video listing and editing

--- plugins/user-administrator/client/videos-controller.js
+++ plugins/user-administrator/client/videos-controller.js
@@ -2,17 +2,20 @@
 	var app = angular.module('userAdminModule');
 
 
-	app.controller("UserAdminListVideosController", ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+	app.controller("UserAdminListVideosController", ['$scope', '$http', '$timeout', '$cookies', 'Video', 'VideoEditPopup',
+	function($scope, $http, $timeout, $cookies, Video, VideoEditPopup) {
 	
 		$scope.currentPage=1;
 //		$scope.filterQuery = null;
 //		$scope.selectableFilters = Filters.$get('video');
 		$scope.timeoutReload = null;
 //		$scope.timeoutSearchText = null;			
-		$scope.state = {
-			itemsPerPage: 10
-		};
+		$scope.itemsPerPage = $cookies.get('itemsPerPage') || '10';
 		
+		
+		$scope.$watch('itemsPerPage', function(value){
+			$cookies.put('itemsPerPage', value);
+		});
 		
 		$scope.reloadVideos = function(){
 			if ($scope.timeoutReload) {
@@ -20,7 +23,7 @@
 			}		
 			$scope.loadingVideos = true;
 			$scope.timeoutReload = $timeout(function() {			
-				$http.get('/rest/plugins/user-administrator/videos?limit='+$scope.state.itemsPerPage+'&skip='+($scope.currentPage-1)*$scope.state.itemsPerPage)
+				$http.get('/rest/plugins/user-administrator/videos?limit='+$scope.itemsPerPage+'&skip='+($scope.currentPage-1)*$scope.itemsPerPage)
 				.then(function successCallback(response) {
 					$scope.videos = response.data;
 					console.log($scope.videos);
@@ -33,7 +36,32 @@
 			});	
 		}
 		
+		$scope.editVideo = function(videoId) {
+			Video.get({ id:videoId }).$promise
+			.then(function(data) {
+				delete(data.slides);
+				delete(data.blackboard);
+				delete(data.source);
+				delete(data.thumbnail);					
+				delete(data.search);
+			
+				VideoEditPopup(data)
+				.then(function(newVideoData) {								
+					newVideoData.id = newVideoData._id;
+					return Video.update(newVideoData).$promise;
+				})
+				.then(function(result) {
+					location.reload();
+				});
+			});
+		};		
+		
+		
 		$scope.$watch('currentPage', function(){ $scope.reloadVideos(); });
+		
+		
+		
+		
 		
 		
 	}]);		
