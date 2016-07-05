8674655d6326fdc60fea6f6934df1bdf44d27eeb
Miguel Escriva <miesgre@gmail.com>
2016-07-05 11:07:54 +0200

    Fix bug

--- repository.js
+++ repository.js
@@ -6,8 +6,10 @@ exports.setup = function(router,app) {
 	Repository.find()
        	.exec(function(err,data) {
        		data.forEach(function(repo) {
-            	if (repo.path[0]=='/') app.use(repo.endpoint,express.static(repo.path));
-            	else app.use(repo.endpoint,express.static(__dirname + '/' + repo.path));
+	       		if (repo.endpoint) {
+	            	if (repo.path[0]=='/') app.use(repo.endpoint,express.static(repo.path));
+	            	else app.use(repo.endpoint,express.static(__dirname + '/' + repo.path));
+	            }
        		});
         });
 }
