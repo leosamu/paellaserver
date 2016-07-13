b7801e03d1a303087846f556ec2351bc8d403807
Miguel Escriva <miesgre@gmail.com>
2016-07-13 13:48:02 +0200

    main admin page now shows logged user details

--- plugins/admin/client/admin-controller.js
+++ plugins/admin/client/admin-controller.js
@@ -2,8 +2,13 @@
 	var app = angular.module('adminModule');
 
 	
-	app.controller("AdminController", ['$scope', 'AdminPlugin', function($scope, AdminPlugin) {
+	app.controller("AdminController", ['$scope', 'AdminPlugin', 'User', function($scope, AdminPlugin, User) {	
 		$scope.plugins = AdminPlugin;
+		
+		User.current().$promise.then(function(currentUser) {
+			$scope.currentUser = currentUser;
+		});
+		
 	}]);
 
 
