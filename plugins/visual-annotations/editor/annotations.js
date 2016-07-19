05706e95c9083624b02eb03b7d8d0eaa084e3212
Leonardo Salom <leosamu@gmail.com>
2016-07-19 10:03:55 +0200

    fixed selection and style bug

--- plugins/visual-annotations/editor/annotations.js
+++ plugins/visual-annotations/editor/annotations.js
@@ -43,18 +43,8 @@
 					annotations.forEach(function(annotation)
 					{
 						var _data = JSON.parse(annotation.content).data[Object.keys(JSON.parse(annotation.content).data)[0]];
-						var _type = "Text"
-						switch (_data.substring(0, 3))
-						{
-							case "<if":
-								_type = "Embed"
-							break;
-							case "<a ":
-								_type = "Link";
-							break;
-							default:
-								_type = "Text";
-						}
+						var _type = annotation['type']
+					
 						self._tracks.push({
 			              id: annotation['_id'],
 			              s: annotation['time'],
@@ -66,7 +56,8 @@
 			              video: annotation['video'],
 	           		      pauser: JSON.parse(annotation.content).pauser,
 			              profile: JSON.parse(annotation.content).profile,			              
-			              style: JSON.parse(annotation.content).style
+			              style: JSON.parse(annotation.content).style,
+			              css: JSON.parse(annotation.content).css
 	            		});				
 	         			
 					});					
@@ -113,6 +104,10 @@
 		allowEditContent() {
 			return true;
 		}
+		
+		setTimeOnSelect() {
+			return true;
+		}
 
 		onTrackChanged(id,start,end) {
 			//base.log.debug('Track changed: id=' + id + ", start: " + start + ", end:" + end);
@@ -125,6 +120,13 @@
 
 		onSelect(trackItemId) {
 			this._currentId = trackItemId;
+			
+			/*this._tracks.some(function(trackItem, i) {
+				console.log(trackItem);
+                if (trackItem.id == trackItemId) {
+                  paella.player.videoContainer.seekToTime(trackItem.s);
+                }
+			});	*/		
 			console.log('Track item selected: ' + this.getTrackName() + ", " + trackItemId);
 		}
 
@@ -133,47 +135,7 @@
 			base.log.debug('Track list unselected: ' + this.getTrackName());
 		}
 		
-		onSave() {
-			/*	$.getJSON('/rest/video/' + paella.initDelegate.getId() + "/annotations",function(data){
-						        
-							              data.forEach(function(annotation)
-											{
-												$.ajax({
-												    url: '/rest/video/' + annotation.video + '/annotation/' + annotation['_id'],
-												    type: 'DELETE',
-												    success: function(result) {
-												        // Do something with the result
-												        console.log(result);
-												    }
-												});
-											});
-							              
-				        })
-				        .then(
-					        self._tracks.forEach(function(track)
-							{
-								var body = {};
-								var annotation = {};
-								annotation['_id'] = track.id;
-								annotation.time = track.s;
-					            annotation.duration =track.e - track.s;
-					            annotation['type'] = track.type;
-					            paella.player.accessControl.userData().then(function(ud) { 
-							        annotation['user']= ud.username; 
-							        annotation['video']= paella.initDelegate.getId();
-						            annotation['title']= track.title;
-						            var _content = {};
-						            _content.pauser = track.pauser;
-						            _content.profile = track.profile;						          
-									_content.style = self.createStyle(track.style,annotation['_id']);
-						            _content.data={}
-						            _content.data.es = track.data;
-						            annotation.content = JSON.stringify(_content); 
-						            body['annotation']=annotation;
-									$.post( '/rest/video/'+ track.video +'/annotation/' + track.id, body);	
-								});
-							})
-				        );	*/	
+		onSave() {						
 			var self = this;
 			return new Promise((resolve,reject) => {				
 				/*managing previous versions of the annotations*/
@@ -189,11 +151,12 @@
 				            annotation['type'] = track.type;				          
 					        annotation['user']= ud.username; 
 					        annotation['video']= paella.initDelegate.getId();
-				            annotation['title']= track.title;
+				            annotation['title']= track.name;
 				            var _content = {};
 				            _content.pauser = track.pauser;
-				            _content.profile = track.profile;						          
-							_content.style = self.createStyle(track.style,annotation['_id']);
+				            _content.profile = track.profile;
+				            _content.style = track.style;						          
+							_content.css = self.createStyle(track.style,annotation['_id']);
 				            _content.data={}
 				            _content.data.es = track.data;
 				            annotation.content = JSON.stringify(_content); 
@@ -248,18 +211,18 @@
 						annotation['_id'] = this.newObjectId();
 						annotation.time = _time;
 			            annotation.duration = 10;
-			            annotation['type'] = "BANNER";
+			            annotation['type'] = "Text";
 			            paella.player.accessControl.userData().then(function(ud) { 
 				        annotation['user']= ud.username; 
 				        annotation['video']= paella.initDelegate.getId();
 			            annotation['title']= "Annotation title";
 			            var _content = {};
 			            _content.pauser = false;
-			            _content.profile = "";
-			            
-			            var _style = "#annotationID {color:rgba(255,255,255,1);background-color: rgba(0,0,0,0.6);text-align: justify;-moz-border-radius: 15px;border-radius: 15px;line-height: 3vmin;font-size: 1.2vw;padding: 1vw;position: absolute;bottom:70px;left:30px;right:30px;} #annotationID:hover { background-color: rgba(0,0,0,0.8);} #annotationID a{color: rgb(255, 230, 45);} #annotationID a:visited{color: rgba(255, 255, 150, 0.80);}".replace(new RegExp("annotationID", 'g'), annotation['_id']);
-						_content.style = _style;
-			            _content.data={}
+			            _content.profile = "";			            
+			            var _css = "#annotationID {color:rgba(255,255,255,1);background-color: rgba(0,0,0,0.6);text-align: justify;-moz-border-radius: 15px;border-radius: 15px;line-height: 3vmin;font-size: 1.2vw;padding: 1vw;position: absolute;bottom:70px;left:30px;right:30px;} #annotationID:hover { background-color: rgba(0,0,0,0.8);} #annotationID a{color: rgb(255, 230, 45);} #annotationID a:visited{color: rgba(255, 255, 150, 0.80);}".replace(new RegExp("annotationID", 'g'), annotation['_id']);
+						_content.css = _css;
+						_content.style = "BANNER";
+			            _content.data={};
 			            _content.data.es = "Your text here";
 			            annotation.content = JSON.stringify(_content); 
 			           
@@ -270,8 +233,8 @@
 						              s: annotation['time'],
 						              e: annotation['time'] + annotation['duration'],
 						              name: annotation['title'],
-						              data: "Your text here",
-						              type: "Text",
+						              data: JSON.parse(annotation.content).data.es,
+						              type: annotation['type'],
 						              user: annotation['user'],
 						              video: annotation['video'],
 				           		      pauser: JSON.parse(annotation.content).pauser,
@@ -335,15 +298,15 @@
 				            annotation['_id'] = $scope.selectedAnnotation.id;
 				            annotation.time = $scope.selectedAnnotation.s;
 				            annotation.duration = $scope.selectedAnnotation.e - $scope.selectedAnnotation.s;
-				            annotation['type'] = $scope.selectedAnnotation.style;
+				            annotation['type'] = $scope.selectedAnnotation.type;
 				            annotation['user']= $scope.selectedAnnotation.user;
 				            annotation['video']= $scope.selectedAnnotation.video;
 				            annotation['title']= $scope.selectedAnnotation.name;
 				            var _content = {};
 				            _content.pauser = $scope.selectedAnnotation.pauser;
 				            _content.profile = $scope.selectedAnnotation.profile;
-				     
-				            _content.style = self.createStyle($scope.selectedAnnotation.style,annotation['_id']);;
+							_content.style = $scope.selectedAnnotation.style;
+				            _content.css = self.createStyle($scope.selectedAnnotation.style,annotation['_id']);;
 				            _content.data={}
 				            _content.data.es = $scope.selectedAnnotation.data;
 				            annotation.content = JSON.stringify(_content); 
@@ -360,10 +323,19 @@
 				PaellaEditor.subscribe($scope,() => {
 					$scope.currentTrack = PaellaEditor.currentTrack;
 					$scope.selectedType="";
-					if (PaellaEditor.currentTrackItem){
-						$scope.selectedAnnotation =PaellaEditor.currentTrackItem.trackData;
+					
+					var currentTrackItem = null;
+					
+					$scope.currentTrack.plugin._tracks.some(function(trackItem, i) {						
+		                if (trackItem.id == $scope.currentTrack.plugin._currentId) {
+		                  currentTrackItem = trackItem;
+		                }
+					});	
+					
+					if (currentTrackItem){
+						$scope.selectedAnnotation =currentTrackItem;//PaellaEditor.currentTrackItem.trackData;
 						$scope.annotationLoad=true;				
-						paella.player.videoContainer.seekToTime(PaellaEditor.currentTrackItem.trackData.s);
+						paella.player.videoContainer.seekToTime(currentTrackItem.s);//PaellaEditor.currentTrackItem.trackData.s);
 						paella.player.play();
 						paella.player.pause();
 						
